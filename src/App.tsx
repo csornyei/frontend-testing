a499a462231aac805f9ca8777bf78641e23a9771
Mate Csornyei <mate@csornyei.com>
2020-09-19 20:35:18 +0200

    Setup base app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+import Controlls from './Components/Controlls';
+import ResultList from './Components/Results/ResultList';
+
+const Container = styled.section`
+    width: 95%;
+    margin-left: auto;
+    margin-right: auto;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <main>
+        <Container>
+          <Controlls />
+        </Container>
+        <Container>
+          <ResultList />
+        </Container>
+      </main>
     </div>
   );
 }
